7df6f1f6a0de3d3cbdfff91cfebfe07feee0c7e1
Ashiq Ahamed <aashiq121205@gmail.com>
2025-02-09 14:25:14 +0530

    Closures

--- Functions/src/main.rs
+++ Functions/src/main.rs
@@ -1,23 +1,39 @@
 fn main() {
-    func();
 
-    // let y = {
+     // let y = {
     //     let x = 7;
     //     x
     // };
    // println!("y: {}", y);
 
+    func();
+
+    // Call the f function
     println!("{}", f({ // => So the parameter is 6 to the f function
         let y = 5;
         y + 1
     }));
+
+    /////// Closures ////////
+    let initial_value: i32 = 10;
+    let add = |a, b| -> i32  { a + b + initial_value};
+    println!("From Add Closure: {}", add(3, 3));
+
+    let mut count: i32 = 0;
+    let mut increment = | | {
+        count += 1;
+        println!("Count: {}", count);
+    };
+
+    increment();
+    increment();
+
 }
 
 
 fn func() {
     println!("From func Function");
 }
-
 
 fn f(x: i32) -> i32 {x + 1}
 
