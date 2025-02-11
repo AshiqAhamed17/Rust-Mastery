9fb18e6fdd673fc1581a013dbe85d00bc87fe449
Ashiq Ahamed <aashiq121205@gmail.com>
2025-02-11 16:36:59 +0530

    Update: borrowing.rs

--- Ownership/src/borrowing.rs
+++ Ownership/src/borrowing.rs
@@ -6,8 +6,30 @@ pub fn run() {
     borrow(my_str2); // Passing the reference to borrow fn
     println!("From Owner: {}", my_str); // This is still valid becoz ownership is just borrowed
 
+    ////// Mutable Borrowing //////
+    let mut str = String::from("Hello, ");
+    println!("From Owner - Before Update: {}", str);
+
+    mut_borrow(&mut str);
+    println!("From the Owner - After: {}", str);
+    
+    let s3 = &mut str;
+    println!("s3: {}", s3);
+    
+    let s4 = &mut str;
+    println!("s4: {}", s4);
+    // println!("s3: {}", s3); ->  Error: This cannot happen as only one borrower can be at a time
+
+
 }
 
 fn borrow(str: &String) {
     println!("String from the borrow fn: {}", str);
+}
+
+// Borrowing and updating the str.
+fn mut_borrow(str: &mut String) {
+    str.push_str("World");
+    println!("From the mut_borrow fn: {}", str);
+
 }
