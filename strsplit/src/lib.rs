e678da1d9d209e2b35e1a4c146b988079d42bdad
AshiqAhamed17 <aashiq121205@gmail.com>
2026-09-20 19:49:19 +0530

    Create strsplit lib

---
+++ strsplit/src/lib.rs
@@ -0,0 +1,42 @@
+#![warn(missing_debug_implementations, rust_2018_idioms, missing_docs)]
+
+#[derive(Debug)]
+pub struct StrSplit<'a> {
+    remainder: &'a str,
+    delimiter: &'a str,
+}
+
+impl StrSplit<'_ >{
+    pub fn new(haystack: &str, delimiter: &str) -> Self {
+        Self {
+            remainder: haystack,
+            delimiter,
+        }
+    }
+}
+
+impl Iterator for StrSplit <'_ >{
+    type Item = &str;
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(next_delim) = self.remainder.find(self.delimiter) {
+            let until_delimiter = &self.remainder[..next_delim];
+            self.remainder = &self.remainder[(next_delim + self.delimiter.len())..];
+            Some(until_delimiter)
+        } else if  self.remainder.is_empty() {
+            None
+        } else {
+            let rest = self.remainder;
+            self.remainder = &[];
+            Some(rest)
+        }
+    }
+}
+
+
+
+#[test] 
+fn it_works() {
+    let haystack = "a b c d";
+    let letters = StrSplit::new(haystack, " ");
+    assert_eq!(letters, vec!["a", "b", "c", "d"].into_iter());
+}
